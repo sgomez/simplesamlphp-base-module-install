ca71c6ed73d35882b0d7949a89acb326fad11a15
Sergio Gómez <sergio@uco.es>
2021-03-23 13:06:37 +0100

    fix: missed methods

--- src/Composer/ModuleInstallerPlugin.php
+++ src/Composer/ModuleInstallerPlugin.php
@@ -24,4 +24,12 @@ class ModuleInstallerPlugin implements PluginInterface
     public function activate(Composer $composer, IOInterface $io)
     {
     }
+
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
 }
